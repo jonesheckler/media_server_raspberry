d2f92f61b496072756ffc2e2d7f773b60e552e92
jones <mw10@mw10.com.br>
2016-01-03 23:52:05 -0200

    enviando arquivo

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# media_server_raspberry
+# Guia de Instalação - Media Center for PI
+Scripts for PI, instalando MiniDLNA, Samba e Cliente Torrents
+
+Após Rodar o Script você pode baixar seus torrents diretamente pelo Raspberry Pi no endereço
+
+http://IP_do_PI:9091/transmission/
+
+usuario: transmission
+senha: transmission
+
+Pode acessar os arquivos do HD Externo diretamente pela rede:
+
+IP_do_PI/disco_usb
+
+# Configuração
+
+Para fazer o download execute seguinte comando
+
+	$ git clone https://github.com/kanelaoka/media_server_raspberry_pi.git
+
+caso não tenha o git instalado execute o seguinte comando
+
+	$ sudo apt-get install git-core
+
+Após fazer download do arquivo, precisa dar permissão de escrita para o mesmo
+para isso execute o seguinte comando
+
+	$ sudo chmod +x media_center.sh
+
+
+Abra o arquivo media_center.sh com seu editor preferido e altere 
+os parâmetros necessários.
+
+	$ nano media_center.sh
+
+edite a variável dirDownload
+altere apenas o nome da pasta, deixe o caminho padrão /var/lib/minidlna/
+
+Esse é o caminho que ira salvar os downloads e também já o caminho para o 
+DLNA reconhecer os filmes e series.
+não se preocupe essa pasta é apenas um apelido, pois os arquivos serão salvos diretamente
+no HD Externo
+
+# Configurar HD Externo
+
+O Script já esta configurado para salvar os arquivos no HD externo ligado ao 
+Raspberry Pi. 
+O script também já irá montar o HD externo e configurar o mesmo no caminho /mnt/ext
+esse caminho pode ser alterado caso queira, basta editar o script conforme a necessidade
+
+
+os outros campos para mudar são referente ao HD Externo
+para isso execute o seguinte comando
+
+	$ sudo blkid
+
+Copie os códigos e cole nas variáveis abaixo
+no meu caso a resposta foi a seguinte
+
+	$ /dev/sda1: LABEL="SANSUNG" UUID="722083C320838D33" TYPE="ntfs
+
+e colei as variáveis da seguinte maneira, altere conforme a sua resposta 
+	$uuid="722083C320838D33"
+	
+	$sistemaArquivo="ntfs"
+	
+	$localDev="/dev/sda1"
+
+Depois de alterar as variáveis, é só rodar o script e buscar uma cerveja.
+
+	$ ./media_center.sh
---
+++ media_center.sh
@@ -0,0 +1,121 @@
+
+##Script para criar um Media Center usando o Raspberry Pi
+ # Autor: Jones Heckler
+ # twitter: @kanelaoka
+ # git: https://github.com/kanelaoka
+### 
+ 
+
+# Usando o sistema operacional Wheezy
+
+# configurar o diretorio de downloads dos torrents
+
+dirDownload="/var/lib/minidlna/Filmes-e-Seriados"
+
+# configurar o UUID do HD externo 
+# descobrir o UUID do HD externo executar o comando
+## sudo blkid
+# copiar UUID e colar na variavel abaixo
+
+uuid="722083C320838D33"
+sistemaArquivo="ntfs"
+localDev="/dev/sda1"
+
+
+
+#### -------- Instalar MiniDLNA ----------####
+
+# Instalar o Minidlna
+
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install minidlna
+
+# auto-atualizar novos filmes
+
+sed -i 's/#inotify=no/inotify=yes/g' /etc/minidlna.conf
+
+
+# iniciar o minidlna
+# Now we start minidlna and we should have a working media server!
+
+service minidlna start
+
+
+
+# Iniciar o Minidlna ao iniciar o sistema
+# Finally, ask minidlna to start up automatically upon boot.
+
+update-rc.d minidlna defaults
+
+
+#apos configurar tudo fazer com que o minidlna force a checagem das pastas sempre que iniciar
+#colocar ao final do arquivo  /etc/rc.local
+
+echo "sudo service minidlna force-reload" >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
+
+
+#### ---------Instalar o Transmission-Deamon ----------####
+
+apt-get -y install transmission-daemon
+
+
+# Para alterar a configuração é necessário parar o daemon
+
+service transmission-daemon stop
+
+# mudando usuario do transmission para root
+
+sed -i 's/USER=debian-transmission/USER=root/g' /etc/init.d/transmission-daemon
+
+#modificar json
+
+sed -i 's/"rpc-whitelist-enabled": true,/"rpc-whitelist-enabled": false,/g' /etc/transmission-daemon/settings.json
+
+
+# mudar o diretorio que guarda os downloads
+
+sed -i "s|\/var\/lib\/transmission-daemon\/downloads|$dirDownload|g" /etc/transmission-daemon/settings.json
+
+
+
+# Com as configurações alteradas reiniciar o daemon
+
+service transmission-daemon start
+
+
+
+
+######-----------Montar HD Externo ------------######
+
+apt-get -y install ntfs-3g ntfs-config gdisk
+mkdir -p /mnt/ext
+mount $localDev /mnt/ext
+
+echo "UUID=$uuid /mnt/ext $sistemaArquivo defaults 0 0" >>  /etc/fstab 
+mount /dev/sda1 /mnt/ext
+
+echo "UUID=$uuid /mnt/ext ntfs defaults 0 0" >>  /etc/fstab 
+
+
+#Criar links para o minidlna achar a pasta dos filmes
+
+ln -s /mnt/ext/Seriados-HD $dirDownload
+
+
+#####--------Acessar o HD Externo pela rede com Samba ----------############
+
+apt-get -y install samba samba-common-bin
+
+
+echo "[disco_usb] \ncomment = Disco USB \npath = /mnt/ext \nread only = No \ncreate mask = 0777 \ndirectory mask = 0777 \nguest only = Yes \nguest ok = Yes" >> /etc/samba/smb.conf
+service samba restart
+
+
+apt-get -y update
+apt-get -y upgrade
+
+reboot
+
+
